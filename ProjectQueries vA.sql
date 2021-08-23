4cd9944c2a429111218a8f40ab0cae24bf14ff8f
npthomas208 <npthomas208@live.com>
2021-08-23 16:27:07 -0400

    SQL query

---
+++ ProjectQueries vA.sql
@@ -0,0 +1,34 @@
+use project;
+
+/*
+1. What were our total sales per quarter last year?
+*/
+/*
+2. Which are our highest revenue-generating products?
+*/
+/*
+3. Who are our largest customers?
+*/
+/*
+4. Which countries are our most important markets?
+*/
+/*
+5. What’s the best-selling product, based on the number of units sold?
+*/
+/*
+6. How many customers are “whales” i.e., have spent, in their lifetime, more than 
+$4,000? How many are “shrimps,” having spent less than $20?
+*/
+/*
+7. Who are the top three suppliers by revenue, and where are they located?
+*/
+/*
+8. What’s the best-selling product, based on the number of times the product has 
+been ordered?
+*/
+/*
+9. Who are our best performing employees in terms of the number of units sold?
+*/
+/*
+10. Who are our best performing employees in terms of the dollar amount sold?
+*/
