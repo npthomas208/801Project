1e9721fc8e86d1e32897e3bcdf8c306608b6c38c
npthomas208 <npthomas208@live.com>
2021-08-23 17:06:31 -0400

    2

--- ProjectQueries vA.sql
+++ ProjectQueries vA.sql
@@ -13,29 +13,60 @@ select quarter(order_date),sum(quantity*unit_price)
 /*
 2. Which are our highest revenue-generating products?
 */
+
+select product_id, product_name,sum(quantity*unit_price) as 'rev'
+	from orders
+	inner join order_details
+    on orders.id = order_details.order_id
+    inner join products
+    on products.id = order_details.product_id
+    group by product_id
+    order by rev desc;
+
 /*
 3. Who are our largest customers?
 */
+
+
+
 /*
 4. Which countries are our most important markets?
 */
+
+
+
 /*
 5. What’s the best-selling product, based on the number of units sold?
 */
+
+
+
 /*
 6. How many customers are “whales” i.e., have spent, in their lifetime, more than 
 $4,000? How many are “shrimps,” having spent less than $20?
 */
+
+
+
 /*
 7. Who are the top three suppliers by revenue, and where are they located?
 */
+
+
+
 /*
 8. What’s the best-selling product, based on the number of times the product has 
 been ordered?
 */
+
+
+
 /*
 9. Who are our best performing employees in terms of the number of units sold?
 */
+
+
+
 /*
 10. Who are our best performing employees in terms of the dollar amount sold?
 */
