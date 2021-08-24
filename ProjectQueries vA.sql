feaebaef74999e49ea0fa8bb23522d79f3f7dbd5
npthomas208 <npthomas208@live.com>
2021-08-23 20:55:00 -0400

    added

--- ProjectQueries vA.sql
+++ ProjectQueries vA.sql
@@ -27,25 +27,54 @@ select product_id, product_name,sum(quantity*unit_price) as 'rev'
 3. Who are our largest customers?
 */
 
-
+select company, sum(quantity*unit_price) as 'rev' 
+	from customers
+	inner join orders
+	on orders.customer_id = customers.id
+    inner join order_details
+    on orders.id = order_details.order_id
+    group by company
+    order by rev desc;
 
 /*
 4. Which countries are our most important markets?
 */
 
-
+select country_region, sum(quantity*unit_price) as 'rev' 
+	from customers
+	inner join orders
+	on orders.customer_id = customers.id
+    inner join order_details
+    on orders.id = order_details.order_id
+    group by country_region
+    order by rev desc;
 
 /*
 5. What’s the best-selling product, based on the number of units sold?
 */
 
-
-
+select product_id, product_name,sum(quantity) as 'rev'
+	from orders
+	inner join order_details
+    on orders.id = order_details.order_id
+    inner join products
+    on products.id = order_details.product_id
+    group by product_name
+    order by rev desc;
+	
 /*
 6. How many customers are “whales” i.e., have spent, in their lifetime, more than 
 $4,000? How many are “shrimps,” having spent less than $20?
 */
 
+select concat(first_name,' ',last_name) as 'name', sum(quantity*unit_price) as 'rev' 
+	from customers
+	inner join orders
+	on orders.customer_id = customers.id
+    inner join order_details
+    on orders.id = order_details.order_id
+    group by name
+    order by rev desc;
 
 
 /*
