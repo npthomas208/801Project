5abb46c10084d96878f9d7dd66b408be343510c2
npthomas208 <npthomas208@live.com>
2021-08-25 13:13:54 -0400

    7

--- ProjectQueries vA.sql
+++ ProjectQueries vA.sql
@@ -92,9 +92,42 @@ select * from
 
 /*
 7. Who are the top three suppliers by revenue, and where are they located?
+
+Assuming we sell all products ordered... I'm assuming we can base revenue on the assume list price in the products table, instead of the order details.unit_price
 */
 
+select customer_order_id from inventory_transactions
+	where customer_order_id is not null;
 
+select type_name from inventory_transactions
+    inner join inventory_transaction_types
+    on inventory_transaction_types.id = inventory_transactions.transaction_type;
+
+/*
+There appears to be exactly 0 waste.
+*/
+
+select suppliers.company,
+	products.product_name,
+    type_name,
+    sum(inventory_transactions.quantity*products.list_price) as 'revenue'
+	from suppliers
+    inner join purchase_orders
+    on suppliers.id = purchase_orders.supplier_id
+    inner join purchase_order_details
+    on purchase_orders.id = purchase_order_details.purchase_order_id
+	inner join inventory_transactions
+    on inventory_transactions.id = purchase_order_details.inventory_id
+	inner join products
+    on products.id = inventory_transactions.product_id
+    inner join inventory_transaction_types
+    on inventory_transaction_types.id = inventory_transactions.transaction_type
+    group by suppliers.company, products.product_name, type_name
+    order by revenue desc;
+
+/*
+Are there multiple suppliers for products?
+*/
 
 /*
 8. What’s the best-selling product, based on the number of times the product has 
